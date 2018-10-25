4b0fedbeffbd22282b60677651c6b157576bbfb0
maddy12 <maddy.schiappa@hotmail.com>
2018-10-24 20:05:03 -0400

    added the construct model from version 2

--- evaluate/evaluation.py
+++ evaluate/evaluation.py
@@ -3,29 +3,52 @@ import torch
 from coco_eval import run_eval
 from rtpose_vgg import get_model, use_vgg
 from torch import load
+import sys
+sys.path.append('..')
+import torch
+import pose_estimation
+import cv2
 
-#Notice, if you using the 
+def construct_model(model_path):
+    model = pose_estimation.PoseModel(num_point=19, num_vector=19)
+    state_dict = torch.load(model_path)['state_dict']
+    from collections import OrderedDict
+    new_state_dict = OrderedDict()
+    for k, v in state_dict.items():
+        name = k
+        new_state_dict[name] = v
+    state_dict = model.state_dict()
+    state_dict.update(new_state_dict)
+    model.load_state_dict(state_dict)
+    # model = model.cuda()
+    model.eval()
+
+    return model
+
+# Notice, if you using the
 with torch.autograd.no_grad():
     # this path is with respect to the root of the project
-    weight_name = r'C:\Users\maddy\OneDrive - Knights - University of Central Florida\CAP5415-KeypointDetection\coco_pose_iter_440000.pth.tar'
-    state_dict = torch.load(weight_name)
-    model = get_model(trunk='vgg19')
-    
+    main_dir = r'C:\Users\maddy\OneDrive - Knights - University of Central Florida\CAP5415-KeypointDetection'
+    model_path = main_dir + r'\coco_pose_iter_440000.pth.tar'
+
+    state_dict = torch.load(model_path)['state_dict']
+    # model = get_model(trunk='vgg19')
+    model = construct_model(model_path)
     model = torch.nn.DataParallel(model).cuda()
     model.load_state_dict(state_dict)
     model.eval()
     model.float()
     model = model.cuda()
-    
+
     # The choice of image preprocessing include: 'rtpose', 'inception', 'vgg' and 'ssd'.
-    # If you use the converted model from caffe, it is 'rtpose' preprocess, the model trained in 
+    # If you use the converted model from caffe, it is 'rtpose' preprocess, the model trained in
     # this repo used 'vgg' preprocess
-    main_dir = r'C:\Users\maddy\OneDrive - Knights - University of Central Florida\CAP5415-KeypointDetection'
-    image_dir = r'\dataset\Coco\images'
-    model_path = 'coco_pose_iter_440000.pth.tar'
-    output_dir = 'results/'
-    run_eval(image_dir= '/data/coco/images/', anno_dir = '/data/coco', vis_dir = '/data/coco/vis',
-        image_list_txt='image_info_val2014_1k.txt',
-        model=model, preprocess='vgg')
+    image_dir = main_dir + r'\dataset\COCO\images'
+    model_path = main_dir + r'\coco_pose_iter_440000.pth.tar'
+    output_dir = r'\results'
+    anno_path = main_dir + r'\dataset\COCO\annotations'
+    run_eval(image_dir=image_dir, anno_dir=anno_path, vis_dir='/data/coco/vis',
+             image_list_txt='image_info_val2014_1k.txt',
+             model=model, preprocess='vgg')
 
 
